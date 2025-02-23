3c30d7a0268c1a0245aae6ac0dffdadfa4b4ce90
Ledenit <georgijkabickij1@gmail.com>
2025-02-23 18:02:57 +0500

    Initialization commit

---
+++ src/main/java/com/night/sinara_learn_java/SinaraLearnProjectApp.java
@@ -0,0 +1,17 @@
+package com.night.sinara_learn_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+
+public class SinaraLearnProjectApp {
+    public static void main(String[] args) {
+        SpringApplication.run(com.night.sinara_learn_java.SinaraLearnProjectApp.class, args);
+    }
+}
+
+
